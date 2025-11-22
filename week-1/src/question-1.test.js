95bafe93a069c1296911af93f022fbcae654cc66
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-11-22 15:13:22 +0430

    change the importing way of function

--- week-1/src/question-1.test.js
+++ week-1/src/question-1.test.js
@@ -1,6 +1,4 @@
-
-const addNumbers = require("./question-1.js");
-
+import addNumbers from "./question-1.js";
 test("should return a number that is the sum of two numbers", () => {
   expect(addNumbers(1, 2)).toBe(3);
 });
